5b426ad3468c4174f64ef334c94a67d496a97468
Karen <karen.miller@northwestern.edu>
2021-03-15 20:32:18 -0500

    homework

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# WeatherDash
+Weather Dash 
+
+The app is deployed here: https://pdjingle.github.io/WeatherDash/
+
+Project Description: 
+
+Within this app you will enter the city name or the city, state within the search field and then press the search button to find 
+the data requested within the search field. The app will display the following properties: temp, humidity, UV, and a photo to represent the 
+current weather conditions. 
+
+The previous search will be shown below to easily access the previous locations. 
---
+++ js.js
@@ -0,0 +1,190 @@
+// /Variable declaired and city is searched and stored
+var city="";
+var searchCity = $("#search-city");
+var currentUvindex= $("#uv-index");
+var searchButton = $("#search-button");
+var currentTemperature = $("#temperature");
+var clearButton = $("#clear-history");
+var currentWSpeed=$("#wind-speed");
+var currentCity = $("#current-city");
+var currentHumidty= $("#humidity");
+var sCity=[];
+
+//The search function to find out if the city is within the database 
+function find(c){
+    for (var i=0; i<sCity.length; i++){
+        if(c.toUpperCase()===sCity[i]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
+//The API Key for openweathermap.org
+var APIKey="b5dc9a1d4c9a7bde4760cf2ef832ed6a";
+
+// Display the curent and future weather to the user after grabing the city form the input text box.
+function displayWeather(event){
+    event.preventDefault();
+    if(searchCity.val().trim()!==""){
+        city=searchCity.val().trim();
+        currentWeather(city);
+    }
+}
+// The function to call the current weather in the desired location 
+function currentWeather(city){
+
+// URL to pull data from website
+    var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + APIKey;
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+
+// Display following: City, current weather, date and a weather icon
+        console.log(response);
+     
+
+//Date format 
+        var date=new Date(response.dt*1000).toLocaleDateString();
+
+//Name of city and date
+        $(currentCity).html(response.name) 
+
+//  display the current temperature with the conversion to fahrenheit 
+
+        var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+        $(currentTemperature).html((tempF).toFixed(2));
+       
+// Current Humidity
+        $(currentHumidty).html(response.main.humidity+"%");
+        
+ //Display MPH Wind Speed Conversion 
+        var ws=response.wind.speed;
+        var windsmph=(ws*2.237).toFixed(1);
+        $(currentWSpeed).html(windsmph+"MPH");
+        
+// Display UVIndex.
+        UVIndex(response.coord.lon,response.coord.lat);
+        forecast(response.id);
+        if(response.cod==200){
+            sCity=JSON.parse(localStorage.getItem("cityname"));
+            console.log(sCity);
+            if (sCity==null){
+                sCity=[];
+                sCity.push(city.toUpperCase()
+                );
+                localStorage.setItem("cityname",JSON.stringify(sCity));
+                addToList(city);
+            }
+            else {
+                if(find(city)>0){
+                    sCity.push(city.toUpperCase());
+                    localStorage.setItem("cityname",JSON.stringify(sCity));
+                    addToList(city);
+                }
+            }
+        }
+
+    });
+}
+// UV Index using String Template Literals, I think
+function UVIndex(lon,lat){
+    var queryURL= `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${APIKey}`
+    $.ajax({
+        url:queryURL,
+        method:"GET",
+    }).then(function(response){
+        // response data to get the result 
+    })
+
+}
+
+// Here we display the 5 days forecast for the current city.
+function forecast(cityid){
+    var dayover= false;
+    var queryforcastURL="https://api.openweathermap.org/data/2.5/forecast?id="+cityid+"&appid="+APIKey;
+    ({
+        url:queryforcastURL,
+        method:"GET"
+    }).then(function(response){
+        
+        for (i=0;i<5;i++){
+            var date= new Date((response.list[((i+1)*8)-1].dt)*1000).toLocaleDateString();
+            var tempK= response.list[((i+1)*8)-1].main.temp;
+            var tempF=(((tempK-273.5)*1.80)+32).toFixed(2);
+            var humidity= response.list[((i+1)*8)-1].main.humidity;
+        
+            $("#fDate"+i).html(date);
+            $("#fImg"+i).html("<img src="+iconurl+">");
+            $("#fTemp"+i).html(tempF);
+            $("#fHumidity"+i).html(humidity+"%");
+        }
+        
+    });
+}
+
+//Past city search data below search box 
+function addToList(c){
+    var listEl= $("<li>"+c.toUpperCase()+"</li>");
+    $(listEl).attr("class","list-group-item");
+    $(listEl).attr("data-value",c.toUpperCase());
+    $(".list-group").append(listEl);
+}
+// Past searched cities 
+function invokePastSearch(event){
+    var liEl=event.target;
+    if (event.target.matches("li")){
+        city=liEl.textContent.trim();
+        currentWeather(city);
+    }
+
+}
+
+// Last load city function
+function loadlastCity(){
+    $("ul").empty();
+    var sCity = JSON.parse(localStorage.getItem("cityname"));
+    if(sCity!==null){
+        sCity=JSON.parse(localStorage.getItem("cityname"));
+        for(i=0; i<sCity.length;i++){
+            addToList(sCity[i]);
+        }
+        city=sCity[i-1];
+        currentWeather(city);
+    }
+
+}
+//Clear the search history from the page
+function clearHistory(event){
+    event.preventDefault();
+    sCity=[];
+    localStorage.removeItem("cityname");
+    document.location.reload();
+}
+//Clicky buttons 
+$("#search-button").on("click",displayWeather);
+$(document).on("click",invokePastSearch);
+$(window).on("load",loadlastCity);
+$("#clear-history").on("click",clearHistory);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
